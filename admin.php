ff4569aadb5f7c527c96cd0267a733c94a0a91dc
meriem-djidel <92149090+meriem-djidel@users.noreply.github.com>
2022-01-15 10:43:50 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 include('connect.php');
 ?>
 <!DOCTYPE html>
@@ -15,40 +16,57 @@ include('connect.php');
 
 <body>
     <header>
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="deconnexion.php">Déconnexion</a></li>
-        </ul>
+
+        <?php
+        if (isset($_SESSION['login']) && $_SESSION['admin'] == TRUE) {
+            echo "      
+                <ul> 
+                    <button><li><a href='index.php'>Accueil</a></li></button>
+                    <button><li><a href='profil.php'>Profil</a></li></button>
+                    <button><li><a href='deconnexion.php'>Se déconnecter</a></li></button>
+                </ul>";
+        }
+        if (isset($_SESSION['admin']) != TRUE) {
+            echo " 
+                <ul>
+                    <button><li><a href='index.php'>Acceuil</a></li></button>
+                    <button><li><a href='inscription.php'>Inscription</a></li></button>
+                    <button><li><a href='connexion.php'>Connexion</a></li></button>
+                </ul>";
+        }
+        ?>
     </header>
     <h1>Admin</h1>
     <?php
-    if ($_SESSION['login'] == 'admin') {
+    if ($_SESSION['login'] == TRUE) {
         $req = "SELECT * FROM utilisateurs ";
         $query = mysqli_query($sql, $req);
     }
     ?>
-    <table border="2">
+
+    <table border="9" id="admin">
         <thead>
             <tr>
-                <th>login</th>
-                <th>prénom</th>
-                <th>nom</th>
-                <th>password</th>
+                <th>Login</th>
+                <th>Prénom</th>
+                <th>Nom</th>
+                <th>Password</th>
             </tr>
         </thead>
         <?php
         while ($result = mysqli_fetch_array($query)) {
         ?>
             <tbody>
-                <td><?php echo $result['login']; ?></td>
-                <td><?php echo $result['prenom']; ?></td>
-                <td><?php echo $result['nom']; ?></td>
+                <td><?php echo $result['login']; ?> </td>
+                <td><?php echo $result['prenom']; ?> </td>
+                <td><?php echo $result['nom']; ?> </td>
                 <td><?php echo $result['password']; ?></td>
             </tbody>
         <?php
         }
         ?>
     </table>
+    </div>
 </body>
 
 </html>
